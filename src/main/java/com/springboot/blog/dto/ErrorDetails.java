9fc9588c3f6cfafdb77d71ea896243b7fb2d9faf
sjagadee <srini.jagadeesh@gmail.com>
2024-08-14 22:35:14 -0500

    global exception handler

---
+++ src/main/java/com/springboot/blog/dto/ErrorDetails.java
@@ -0,0 +1,14 @@
+package com.springboot.blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+}
